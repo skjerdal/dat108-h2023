138ab33a013e1f6f31b4f8efe425c4c3d17b4117
Lars-Petter Helland <lph@hvl.no>
2023-09-08 14:06:11 +0200

    Løsningsforslag til f06

---
+++ f06-eksempler-solution/src/no/hvl/dat108/f06c/_SleepStoppJoinMain.java
@@ -0,0 +1,28 @@
+package no.hvl.dat108.f06c;
+
+public class _SleepStoppJoinMain {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		/*
+		 * I dette eksempelet skal vi se på
+		 * - Hvordan legge inn pause i en tråd
+		 * - Hvordan stoppe en tråd som ellers går "evig"
+		 * - Hvordan vente på at en tråd gjør seg ferdig
+		 * - Hvordan avslutte program selv om ikke alle tråder er ferdige
+		 */
+		
+		TraadSomKanStoppes t = new TraadSomKanStoppes("hei_tråd");
+		
+		t.setDaemon(true);
+		t.start();
+		
+		Thread.sleep(5000);
+		
+		t.stopp();
+		
+		t.join();
+		
+		System.out.println(Thread.currentThread().getName() + " avsluttes");
+	}
+}
---
+++ f06-eksempler-solution/src/no/hvl/dat108/f06d/TelleOppTraad.java
@@ -0,0 +1,20 @@
+package no.hvl.dat108.f06d;
+
+public class TelleOppTraad extends Thread {
+	
+	private Teller teller;
+	private int n;
+	
+	public TelleOppTraad(Teller teller, int n) {
+		this.teller = teller;
+		this.n = n;
+	}
+	
+	@Override
+	public void run() {
+		for (int i=0; i<n; i++) {
+			//Kan gjøre synchronized(teller) her i stedet for i Teller
+			teller.tellOpp();
+		}
+	}
+}
